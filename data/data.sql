57f099e1b881b38d8b1c19c81a5a2ab8e1588130
Mac1410 <maciejbabral@student.agh.edu.pl>
2026-01-30 17:57:51 +0100

    feat: Wypełnienie tabeli homework

--- data/data.sql
+++ data/data.sql
@@ -226,3 +226,17 @@ INSERT INTO tests (ttid, title, description) VALUES
 (20, 'Sprawdzian z programowania w Pythonie', 'Zadania z instrukcji warunkowych i pętli.'),
 (21, 'Test z funkcji liniowych', 'Zagadnienia: wykresy, miejsca zerowe, współczynniki kierunkowe.'),
 (22, 'Sprawdzian z języka angielskiego', 'Zakres: czasy gramatyczne, słownictwo związane z podróżowaniem.');
+
+-- Tabela zadań domowych
+
+INSERT INTO homework (cid, tid, sbid, title, description) VALUES
+(3, 1, 1, 'Zadania z równań liniowych', 'Rozwiąż 5 równań z jedną niewiadomą.'),
+(7, 2, 2, 'Zasady dynamiki Newtona', 'Opisz trzy zasady dynamiki i podaj przykłady.'),
+(12, 3, 3, 'Reakcje chemiczne', 'Zapisz równania reakcji dla 5 przykładów.'),
+(5, 4, 4, 'Budowa komórki roślinnej', 'Narysuj komórkę i opisz jej organelle.'),
+(9, 5, 5, 'Algorytmy w pseudokodzie', 'Zapisz algorytm obliczania średniej arytmetycznej.'),
+(14, 6, 6, 'Powstanie listopadowe', 'Przygotuj notatkę o przyczynach i skutkach powstania.'),
+(2, 7, 7, 'Mapa fizyczna Polski', 'Zaznacz na mapie główne pasma górskie i rzeki.'),
+(17, 8, 8, 'Analiza tekstu literackiego', 'Zinterpretuj wiersz „Deszcz jesienny” Leopolda Staffa.'),
+(10, 1, 9, 'Czasy teraźniejsze w języku angielskim', 'Ułóż 10 zdań w Present Simple i Present Continuous.'),
+(6, 2, 10, 'Opis rodziny po niemiecku', 'Napisz krótki tekst o swojej rodzinie z użyciem czasownika „haben”.');
