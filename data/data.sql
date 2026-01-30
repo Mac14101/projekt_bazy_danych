9cdb186e46fed10a4b5002f4faabb8c4e9f3e98b
Mac1410 <maciejbabral@student.agh.edu.pl>
2026-01-30 15:13:00 +0100

    feat: Dodanie przykładowych lekcji

--- data/data.sql
+++ data/data.sql
@@ -126,29 +126,57 @@ INSERT INTO subjects (name) VALUES
 
 -- Tabela planu zajęć
 
-INSERT INTO time_table (date, start_time, end_time, cid, tid, sbid) VALUES
-('2026-01-02', '08:00:00', '08:45:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-02', '08:50:00', '09:35:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-05', '09:45:00', '10:30:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-07', '10:40:00', '11:25:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-07', '11:35:00', '12:20:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-08', '12:40:00', '13:25:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-09', '13:35:00', '14:20:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-09', '14:30:00', '15:15:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-12', '15:20:00', '16:05:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-13', '08:00:00', '08:45:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-13', '09:45:00', '10:30:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-14', '09:45:00', '10:30:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-15', '08:00:00', '08:45:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-15', '11:35:00', '12:20:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-15', '14:30:00', '15:15:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-16', '13:35:00', '14:20:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-19', '14:30:00', '15:15:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-20', '15:20:00', '16:05:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-21', '08:00:00', '08:45:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-22', '08:50:00', '09:35:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-22', '09:45:00', '10:30:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-23', '10:40:00', '11:25:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-26', '11:35:00', '12:20:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-27', '12:40:00', '13:25:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1)),
-('2026-01-28', '13:35:00', '14:20:00', (SELECT cid FROM classes ORDER BY RANDOM() LIMIT 1), (SELECT uid FROM users WHERE role='teacher' ORDER BY RANDOM() LIMIT 1), (SELECT sbid FROM subjects ORDER BY RANDOM() LIMIT 1));
+INSERT INTO time_table (date, start_time, end_time, cid, tid, sbid, canceled, moved) VALUES
+    ('2026-01-02', '08:00:00', '08:45:00', 6, 4, 2, DEFAULT, DEFAULT),
+    ('2026-01-02', '08:50:00', '09:35:00', 8, 33, 11, DEFAULT, DEFAULT),
+    ('2026-01-06', '09:45:00', '10:30:00', 10, 18, 9, DEFAULT, DEFAULT),
+    ('2026-01-07', '10:40:00', '11:25:00', 12, 33, 13, DEFAULT, DEFAULT),
+
+    -- canceled = true
+    ('2026-01-08', '11:35:00', '12:20:00', 10, 7, 5, TRUE, DEFAULT),
+
+    ('2026-01-08', '12:40:00', '13:25:00', 2, 4, 10, DEFAULT, DEFAULT),
+    ('2026-01-09', '13:35:00', '14:20:00', 6, 40, 9, DEFAULT, DEFAULT),
+    ('2026-01-09', '14:30:00', '15:15:00', 6, 26, 2, DEFAULT, DEFAULT),
+    ('2026-01-13', '15:20:00', '16:05:00', 14, 4, 13, DEFAULT, DEFAULT),
+
+    -- moved = true
+    ('2026-01-13', '08:00:00', '08:45:00', 15, 26, 2, DEFAULT, TRUE),
+
+    ('2026-01-14', '09:45:00', '10:30:00', 2, 7, 6, DEFAULT, DEFAULT),
+    ('2026-01-14', '09:45:00', '10:30:00', 5, 13, 4, DEFAULT, DEFAULT),
+    ('2026-01-15', '08:00:00', '08:45:00', 15, 4, 11, DEFAULT, DEFAULT),
+    ('2026-01-15', '11:35:00', '12:20:00', 8, 33, 3, DEFAULT, DEFAULT),
+    ('2026-01-16', '14:30:00', '15:15:00', 16, 7, 12, DEFAULT, DEFAULT),
+    ('2026-01-16', '13:35:00', '14:20:00', 17, 50, 3, DEFAULT, DEFAULT),
+
+    -- canceled = true
+    ('2026-01-19', '14:30:00', '15:15:00', 16, 7, 7, TRUE, DEFAULT),
+
+    ('2026-01-20', '15:20:00', '16:05:00', 18, 26, 10, DEFAULT, DEFAULT),
+    ('2026-01-21', '08:00:00', '08:45:00', 10, 40, 6, DEFAULT, DEFAULT),
+    ('2026-01-21', '08:50:00', '09:35:00', 13, 7, 5, DEFAULT, DEFAULT),
+    ('2026-01-22', '09:45:00', '10:30:00', 2, 33, 1, DEFAULT, DEFAULT),
+    ('2026-01-22', '10:40:00', '11:25:00', 15, 4, 9, DEFAULT, DEFAULT),
+    ('2026-01-26', '11:35:00', '12:20:00', 15, 4, 8, DEFAULT, DEFAULT),
+    ('2026-01-27', '12:40:00', '13:25:00', 6, 18, 9, DEFAULT, DEFAULT),
+    ('2026-01-28', '13:35:00', '14:20:00', 7, 33, 11, DEFAULT, DEFAULT);
+
+
+
+
+-- Tabela lekcji
+
+INSERT INTO lessons (ttid, topic, description) VALUES
+(1, 'Zasady dynamiki Newtona', 'Omówienie trzech zasad dynamiki i ich zastosowanie w życiu codziennym.'),
+(2, 'Systemy polityczne świata', 'Porównanie demokracji, monarchii i systemów autorytarnych.'),
+(3, 'Czasy przeszłe w języku angielskim', 'Ćwiczenia z Past Simple i Past Continuous.'),
+(4, 'Ćwiczenia ogólnorozwojowe z piłką', 'Zajęcia praktyczne z wykorzystaniem piłek lekarskich i gimnastycznych.'),
+(6, 'Przedstawianie siebie i innych po niemiecku', 'Ćwiczenia z użyciem czasownika „sein” i „haben”.'),
+(7, 'Rozumienie tekstu czytanego', 'Analiza krótkich tekstów informacyjnych w języku angielskim.'),
+(8, 'Zasady zachowania energii', 'Omówienie energii kinetycznej i potencjalnej oraz ich przemian.'),
+(9, 'Ćwiczenia z równowagi i koordynacji', 'Zajęcia ruchowe z wykorzystaniem drabinek i równoważni.'),
+(10, 'Prąd elektryczny i napięcie', 'Wyjaśnienie pojęć napięcia, natężenia i oporu elektrycznego.'),
+(11, 'Fotosynteza i budowa liścia', 'Analiza procesu fotosyntezy oraz struktury liścia.'),
+(12, 'Polska w okresie rozbiorów', 'Omówienie sytuacji politycznej i społecznej w XVIII wieku.'),
+(14, 'Budowa atomu', NULL);
